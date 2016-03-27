43316930aaa951c8960bd393b14d1dcd783fd131
Douglas Gaskell <douglasg14b@gmail.com>
2016-03-27 00:30:02 +0000

    Removed unnecessary spaces and added missing semi-colins

--- userscript.js
+++ userscript.js
@@ -15,13 +15,11 @@ var exampleUser = {
 (function AddLibraries(){
     var script = document.createElement("script");
     script.setAttribute("src", "https://ajax.googleapis.com/ajax/libs/angularjs/1.5.0/angular.min.js");
-    document.body.appendChild(script);  
-    
+    document.body.appendChild(script);
     window.setTimeout(Main, 5000);
 })();
 
 function Main(){
-    
             //All the HTML that needs to be inserted
             $('body').prepend(
             '<div id="userStatuses" title="{{statuses.statusList[statuses.selectedStatus].name}} Timers" style="overflow-y:auto;">'+
@@ -38,12 +36,10 @@ function Main(){
                             '<td style="text-align: center; border-color:#dddddd; padding-top: 2px !important; padding-bottom:2px !important; height: auto;">{{user.timeInStatus | date: "H:mm:ss": "UTC"}}</td>'+
                         '</tr>'+
                     '</tbody>'+
-                '</table></div>' ); 
-                $('#userStatuses').dialog();  
-                
+                '</table></div>' );
+                $('#userStatuses').dialog();
                 $('#userStatuses').parent().attr("ng-app", "statusesApp");
                 $('#userStatuses').parent().attr("ng-controller", "statusesAppController");
-                
                 $('#userStatuses').parent().append(
                     '<div style="color: white;" id="settingsAccordian">'+
                         '<h3 style="border: 0px; text-align:center;">Settings</h3>'+
@@ -54,7 +50,7 @@ function Main(){
                             '<label for="hideMatching">Hide Matching:</label>'+
                             '<input type="text" ng-model="hideMatchingText" id="hideMatching" style="width: 40%; font-size: 1em; display:inline-block;">'+
                             '<label for="hideMatching">Highest {{statuses.statusList[statuses.selectedStatus].name}} Permitted (s)</label>'+
-                            '<input type="number" ng-model="statuses.statusList[statuses.selectedStatus].maxTime" id="hideMatching" style="width: 40%; font-size: 1em;">'+                            
+                            '<input type="number" ng-model="statuses.statusList[statuses.selectedStatus].maxTime" id="hideMatching" style="width: 40%; font-size: 1em;">'+
                         '</div>'+
                     '</div>');
                 $('#settingsAccordian').accordion({
@@ -62,14 +58,11 @@ function Main(){
                     active: false,
                     icons: false
                 });
-                    
-    
     var statusesApp = angular.module("statusesApp", []);
-    
     statusesApp.factory('Statuses', function(){
         var statuses = {};
         statuses.selectedStatus = 'after_call_work';
-        //Simple array for filtering/displaying 
+        //Simple array for filtering/displaying
         statuses.simpleStatusList = [
             {
                 name:"Available",
@@ -79,22 +72,18 @@ function Main(){
             {
                 name:"Followup",
                 id:"after_call_work",
-              
             },
             {
                 name:"HQ Shift Lead",
                 id:"busy_hq-shiftlead",
-                  
             },
             {
                 name:"HQ MC/Texts",
                 id:"busy_hq-mctexts",
-             
             },
             {
                 name:"Dasher Chat",
                 id:"busy_dasherchat",
-               
             },
             {
                 name:"Customer Emails",
@@ -103,13 +92,11 @@ function Main(){
             {
                 name:"Dasher Emails",
                 id:"busy_dasheremails",
-             
             },
             {
                 name:"On a Call",
                 id:"busy",
-            
-            },            
+            },
             {
                 name:"Login Prep",
                 id:"away",
@@ -125,7 +112,7 @@ function Main(){
             {
                 name:"Break",
                 id:"away_break3",
-            },            
+            },
             {
                 name:"Lunch",
                 id: "away_lunch",
@@ -133,7 +120,7 @@ function Main(){
             {
                 name:"Non-Billable",
                 id:"away_nonbillable",
-            },            
+            },
             {
                 name:"Feedback/Coaching/Meeting",
                 id: "away_feedbackcoachingmeeting",
@@ -141,7 +128,7 @@ function Main(){
             {
                 name:"Offline",
                 id:"offline",
-            }            
+            }
         ];
         statuses.statusList = {
             'available':{
@@ -155,111 +142,109 @@ function Main(){
                 name:"Followup",
                 id:"after_call_work",
                 color: true,
-                exactMatch: true,                
-                maxTime: 180                
+                exactMatch: true,
+                maxTime: 180
             },
             'busy_hq-shiftlead':{
                 name:"HQ Shift Lead",
                 id:"busy_hq-shiftlead",
                 color: false,
-                exactMatch: true,                
-                maxTime: -1                        
+                exactMatch: true,
+                maxTime: -1
             },
             'busy_hq-mctexts':{
                 name:"HQ MC/Texts",
                 id:"busy_hq-mctexts",
                 color: false,
-                exactMatch: true,                
-                maxTime: -1                
+                exactMatch: true,
+                maxTime: -1
             },
             'busy_dasherchat':{
                 name:"Dasher Chat",
                 id:"busy_dasherchat",
                 color: false,
-                exactMatch: true,                
-                maxTime: -1                 
+                exactMatch: true,
+                maxTime: -1
             },
             'busy_customeremails':{
                 name:"Customer Emails",
                 id:"busy_customeremails",
                 color: false,
-                exactMatch: true,                
-                maxTime: -1                
+                exactMatch: true,
+                maxTime: -1
             },
             'busy_dasheremails':{
                 name:"Dasher Emails",
                 id:"busy_dasheremails",
                 color: false,
-                exactMatch: true,                
-                maxTime: -1                
+                exactMatch: true,
+                maxTime: -1
             },
             'busy':{
                 name:"On a Call",
                 id:"busy",
                 color: true,
-                exactMatch: true,                
-                maxTime: 300              
-            },            
+                exactMatch: true,
+                maxTime: 300
+            },
             'away':{
                 name:"Login Prep",
                 id:"away",
                 color: true,
-                exactMatch: true,                
-                maxTime: 60                
+                exactMatch: true,
+                maxTime: 60
             },
             'away_break1':{
                 name:"Break",
                 id:"away_break",
                 color: true,
-                exactMatch: false,                
-                maxTime: 1080                
+                exactMatch: false,
+                maxTime: 1080
             },
             'away_break2':{
                 name:"Break",
                 id:"away_break",
                 color: true,
-                exactMatch: false,                
-                maxTime: 1080                
+                exactMatch: false,
+                maxTime: 1080
             },
             'away_break3':{
                 name:"Break",
                 id:"away_break",
                 color: true,
-                exactMatch: false,                
-                maxTime: 1080                
-            },            
+                exactMatch: false,
+                maxTime: 1080
+            },
             'away_lunch':{
                 name:"Lunch",
                 id: "away_lunch",
                 color: true,
-                exactMatch: true,                
-                maxTime: 2100                
+                exactMatch: true,
+                maxTime: 2100
             },
             'away_nonbillable':{
                 name:"Non-Billable",
                 id:"away_nonbillable",
                 color: false,
-                exactMatch: true,                
-                maxTime: -1                 
-            },            
+                exactMatch: true,
+                maxTime: -1
+            },
             'away_feedbackcoachingmeeting':{
                 name:"Feedback/Coaching/Meeting",
                 id: "away_feedbackcoachingmeeting",
                 color: false,
-                exactMatch: true,                
-                maxTime: -1                
+                exactMatch: true,
+                maxTime: -1
             },
             'offline':{
                 name:"Offline",
                 id:"offline",
                 color: false,
-                exactMatch: true,                
-                maxTime: -1                
-            }            
+                exactMatch: true,
+                maxTime: -1
+            }
         };
-        
         statuses.usersHash = {};
-        
         statuses.ProcessStatusChange = function(requestObject){
             //If user exists in hash already
             if(typeof statuses.usersHash[requestObject._id] !== 'undefined'){
@@ -283,7 +268,7 @@ function Main(){
                 console.info(statuses.usersHash);
             }
         };
-        
+
         statuses.CalculateStatusTimes = function(){
             for(var user in statuses.usersHash){
             	if(statuses.usersHash.hasOwnProperty(user)){
@@ -298,24 +283,20 @@ function Main(){
 	                catch(e){
 	                    console.error("Unable To find status's maxTime: " + statuses.usersHash[user].currentStatus, e);
 	                }
-	                 
+
 	                statuses.usersHash[user].timeInStatus = diff;
-	                statuses.usersHash[user].hue = hue;       
+	                statuses.usersHash[user].hue = hue;
 	                statuses.usersHash[user].level = level;
             	}
             }
         };
-        
         return statuses;
-        
     });
-    
+
     //Angular controller for the app
     statusesApp.controller("statusesAppController", ["$scope", "Statuses", function($scope, Statuses){
         $scope.statuses = Statuses;
-        
         $scope.hideMatchingText = "Nothing";
-        
         //The handler for AJAX requests
         $scope.SetupAJAXHandler = function(open) {
             console.info("inside request event handler");
@@ -326,34 +307,34 @@ function Main(){
                         if(typeof this.responseText != 'undefined' && this.responseText != ""){
                             var responseObject = {};
                             try{
-                                responseObject = JSON.parse(this.responseText);                    
+                                responseObject = JSON.parse(this.responseText);
                             }
                             catch(e) {
-                                console.error("JSON Parse Failed")
+                                console.error("JSON Parse Failed");
                            }
                             if(typeof responseObject._id != 'undefined'){
                                 if(typeof responseObject.status != 'undefined'){
-                                    $scope.statuses.ProcessStatusChange(responseObject);  
-                                    console.info("New Valid Request");                                    
+                                    $scope.statuses.ProcessStatusChange(responseObject);
+                                    console.info("New Valid Request");
                                 }
                             }
-                        }                
+                        }
                     }
                 }, false);
                 open.apply(this, arguments);
             };
-        }; 
-        
+        };
+
         //Sets the timer for when times are recalcualted, 500ms at the moment
         $scope.SetupTimer = function(){
             var localScope = $scope;
             setInterval(function(){localScope.statuses.CalculateStatusTimes(); localScope.$apply();}, 500);
-        }
-        
+        };
+
         $scope.SetupAJAXHandler(XMLHttpRequest.prototype.open);
         $scope.SetupTimer();
     }]);
-    
+
     //Converts an associative array to an array https://github.com/petebacondarwin/angular-toArrayFilter
     statusesApp.filter('toArray', function () {
       return function (obj, addKey) {
@@ -371,8 +352,8 @@ function Main(){
           });
         }
       };
-    });  
-    
+    });
+
     /**
      * https://github.com/angular-ui/angular-ui-OLDREPO/blob/master/modules/filters/unique/unique.js
      * Filters out all duplicate items from an array by checking the specified key
@@ -382,16 +363,16 @@ function Main(){
      * @return {array}
      */
     statusesApp.filter('unique', function () {
-    
+
       return function (items, filterOn) {
-    
+
         if (filterOn === false) {
           return items;
         }
-    
+
         if ((filterOn || angular.isUndefined(filterOn)) && angular.isArray(items)) {
           var hashCheck = {}, newItems = [];
-    
+
           var extractValueToCompare = function (item) {
             if (angular.isObject(item) && angular.isString(filterOn)) {
               return item[filterOn];
@@ -399,10 +380,10 @@ function Main(){
               return item;
             }
           };
-    
+
           angular.forEach(items, function (item) {
             var valueToCheck, isDuplicate = false;
-    
+
             for (var i = 0; i < newItems.length; i++) {
               if (angular.equals(extractValueToCompare(newItems[i]), extractValueToCompare(item))) {
                 isDuplicate = true;
@@ -412,15 +393,13 @@ function Main(){
             if (!isDuplicate) {
               newItems.push(item);
             }
-    
+
           });
           items = newItems;
         }
         return items;
       };
-    });    
-    
-    
-    
+    });
+
     angular.bootstrap($('#userStatuses').parent(), ['statusesApp']);
 }
